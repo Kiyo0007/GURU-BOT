e4560789e899a900283ff71f3d6b121d3c0310c3
Kiyo0007 <151784750+Kiyo0007@users.noreply.github.com>
2023-11-23 02:41:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919123708017', 'kiyo', true],
+  ['15593582839', 'ruto', true], 
   [''] 
 ] //Number of owners
 
